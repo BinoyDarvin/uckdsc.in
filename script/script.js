fdfb8b5ed66a38572f7d656f6eb5f88395a3b6ae
Binoy C Darvin <BinoyDarvin@github.com>
2018-06-21 06:47:47 +0530

    First file upload

---
+++ script/script.js
@@ -0,0 +1,207 @@
+function theRevealer() {
+    // progressbar.js@1.0.0 version is used
+    // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+    var bar = new ProgressBar.Circle(roller_one, {
+
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 1,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        easing: 'easeInOut',
+        duration: 1400,
+        svgStyle: null,
+        text: {
+            value: '',
+            alignToBottom: false
+        },
+        from: {
+            color: '#FFEA82'
+        },
+        to: {
+            color: '#ED6A5A'
+        },
+        // Set default step function for all animate calls
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+            circle.path.setAttribute('stroke-width', state.width);
+
+            var value = Math.round(circle.value() * 100);
+            if (value === 0) {
+                circle.setText('');
+            } else {
+                circle.setText(parseInt(value * 100));
+            }
+
+        }
+    });
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    bar.text.style.fontSize = '2rem';
+
+    bar.animate(1.0); // Number from 0.0 to 1.0
+
+
+
+
+
+
+
+
+
+    // progressbar.js@1.0.0 version is used
+    // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+    var bar = new ProgressBar.Circle(roller_two, {
+
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 1,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        easing: 'easeInOut',
+        duration: 1400,
+        svgStyle: null,
+        text: {
+            value: '',
+            alignToBottom: false
+        },
+        from: {
+            color: '#FFEA82'
+        },
+        to: {
+            color: '#ED6A5A'
+        },
+        // Set default step function for all animate calls
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+            circle.path.setAttribute('stroke-width', state.width);
+
+            var value = Math.round(circle.value() * 100);
+            if (value === 0) {
+                circle.setText('');
+            } else {
+                circle.setText(parseInt(value / 20));
+
+            }
+
+        }
+    });
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    bar.text.style.fontSize = '2rem';
+
+    bar.animate(1.0); // Number from 0.0 to 1.0
+
+
+
+
+
+
+
+
+    // progressbar.js@1.0.0 version is used
+    // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+    var bar = new ProgressBar.Circle(roller_three, {
+
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 1,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        easing: 'easeInOut',
+        duration: 1400,
+        svgStyle: null,
+        text: {
+            value: '',
+            alignToBottom: false
+        },
+        from: {
+            color: '#FFEA82'
+        },
+        to: {
+            color: '#ED6A5A'
+        },
+        // Set default step function for all animate calls
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+            circle.path.setAttribute('stroke-width', state.width);
+
+            var value = Math.round(circle.value() * 100);
+            if (value === 0) {
+                circle.setText('');
+            } else {
+                circle.setText(parseInt(value / 5));
+
+            }
+
+        }
+    });
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    bar.text.style.fontSize = '2rem';
+
+    bar.animate(1.0); // Number from 0.0 to 1.0
+
+
+
+
+
+
+
+
+
+    var bar = new ProgressBar.Circle(roller_four, {
+
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 1,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        easing: 'easeInOut',
+        duration: 1400,
+        svgStyle: null,
+        text: {
+            value: '',
+            alignToBottom: false
+        },
+        from: {
+            color: '#FFEA82'
+        },
+        to: {
+            color: '#ED6A5A'
+        },
+        // Set default step function for all animate calls
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+            circle.path.setAttribute('stroke-width', state.width);
+
+            var value = Math.round(circle.value() * 100);
+            if (value === 0) {
+                circle.setText('');
+            } else {
+                circle.setText(parseInt(value / 10));
+            }
+
+
+        }
+    });
+    bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+    bar.text.style.fontSize = '2rem';
+    // circle.setText(25);
+
+    bar.animate(1.0); // Number from 0.0 to 1.0
+
+
+}
+
+
+
+
+
+
+
+$('.roller-container').whenInViewport(theRevealer);
